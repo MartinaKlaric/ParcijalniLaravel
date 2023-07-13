4597227ef59c5751e39266f2a848a660dd5ee03e
Martina Klarić <martinaklaric1103gmail.com>
2023-07-13 18:40:36 +0000

    parcijalni ispit Laravel

---
+++ resources/views/parcijalni/parcijalni.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parcijalni Ispit</title>
+</head>
+<body>
+    <p>Parcijalni ispit Laravel</p>   
+</body>
+</html>
